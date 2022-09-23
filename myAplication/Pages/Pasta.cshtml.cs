099a4ae47f19e455443bb813e102250cb75abd98
CapitanAnna <capitanstil@gmail.com>
2022-09-23 19:23:35 +0300

    Create razor page. named Pasta in folder Pages

---
+++ myAplication/Pages/Pasta.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace myAplication.Pages
+{
+    public class PastaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
