4274144d57f30d0c8034a5ade4a7eb23bf239678
CapitanAnna <capitanstil@gmail.com>
2022-09-23 19:43:04 +0300

    In folder Checkout create two razor pages named Checkout and Thanks. Also, create rezor page CustomPasta in folder Foms

---
+++ myAplication/Pages/Forms/CustomPasta.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace myAplication.Pages.Forms
+{
+    public class CustomPastaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
