de515cf87f8b792a0061f1f65584e6cb0ea7bfd9
CapitanAnna <capitanstil@gmail.com>
2022-09-24 22:19:02 +0300

    Create a method to calculate a total price of pasta

--- myAplication/Pages/Forms/CustomPasta.cshtml.cs
+++ myAplication/Pages/Forms/CustomPasta.cshtml.cs
@@ -8,8 +8,35 @@ namespace myAplication.Pages.Forms
     {
         [BindProperty]
         public PastasModel Pasta { get; set; }
+        public float PastaPrices { get; set; }
         public void OnGet()
         {
+        }
+        public IActionResult OnPost()
+        {
+            PastaPrices = Pasta.BasePrice;
+            if (Pasta.TomatoSauce)
+            {
+                Pasta.TotalPrice += 20;
+
+            }
+            if (Pasta.Cheese)
+            {
+                Pasta.TotalPrice += 30;
+            }
+            if (Pasta.Tuna)
+            {
+                Pasta.TotalPrice += 40;
+            }
+            if (Pasta.Ham)
+            {
+                Pasta.TotalPrice += 35;
+            }
+            if (Pasta.Mushroom)
+            {
+                Pasta.TotalPrice += 20;
+            }
+            return RedirectToPage("/Checkout/Checkout", new {Pasta.PastaName, PastaPrices});
         }
     }
 }
