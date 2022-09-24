e417d5046c88f5efe9dcd3e07fdec489b43fb023
CapitanAnna <capitanstil@gmail.com>
2022-09-24 21:51:31 +0300

    Create model Pasta, complete view model and filled view

---
+++ myAplication/Models/PastasModel.cs
@@ -0,0 +1,15 @@
+﻿namespace myAplication.Models
+{
+    public class PastasModel
+    {
+        public string ImageTitle { get; set; }
+        public string PastaName { get; set; }
+        public float BasePrice { get; set; } = 70;
+        public bool TomatoSauce { get; set; }
+        public bool Cheese { get; set; }
+        public bool Mushroom { get; set; }
+        public bool Ham { get; set; }
+        public bool Tuna { get; set; }
+        public float TotalPrice { get; set; }
+    }
+}
--- myAplication/Pages/Forms/CustomPasta.cshtml.cs
+++ myAplication/Pages/Forms/CustomPasta.cshtml.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using myAplication.Models;
 
 namespace myAplication.Pages.Forms
 {
     public class CustomPastaModel : PageModel
     {
+        [BindProperty]
+        public PastasModel Pasta { get; set; }
         public void OnGet()
         {
         }
