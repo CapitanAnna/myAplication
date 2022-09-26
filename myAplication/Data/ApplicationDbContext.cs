e2467560a00f4111e43647446109029ad5e7d14d
CapitanAnna <capitanstil@gmail.com>
2022-09-26 13:18:06 +0300

    Add folder to the project and create a class. Also add connectionn to database

---
+++ myAplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using myAplication.Models;
+namespace myAplication.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<PastaOrder> PastaOrders { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+    }
+}
