c4ac07a0f6bcdf88ece207dfce758e28106248ab
CapitanAnna <capitanstil@gmail.com>
2022-09-26 13:23:19 +0300

    Add migration. connect to my DB

---
+++ myAplication/Migrations/20220926102135_addedPastaOrder.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace myAplication.Migrations
+{
+    public partial class addedPastaOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PastaOrders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PastaName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BasePrice = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PastaOrders", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PastaOrders");
+        }
+    }
+}
