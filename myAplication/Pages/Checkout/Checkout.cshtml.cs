c489af4f07353f6a25f1c910fb4b122c24ad37a9
CapitanAnna <capitanstil@gmail.com>
2022-09-26 14:39:47 +0300

    Add pasta to my DB

--- myAplication/Pages/Checkout/Checkout.cshtml.cs
+++ myAplication/Pages/Checkout/Checkout.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using myAplication.Data;
+using myAplication.Models;
 
 namespace myAplication.Pages.Checkout
 {
@@ -11,16 +13,29 @@ namespace myAplication.Pages.Checkout
         public float PastaPrice { get; set; }
         public string ImageTitle { get; set; }
 
+        private readonly ApplicationDbContext _context;
+        public CheckoutModel(ApplicationDbContext context)
+        {
+            _context=context;   
+        }
+
         public void OnGet()
         {
             if (string.IsNullOrWhiteSpace(PastaName))
             {
-                PastaName = "customPasta";
+                PastaName = ImageTitle;
             }
             if (string.IsNullOrWhiteSpace(ImageTitle))
             {
                 ImageTitle = "customPasta";
             }
+
+            PastaOrder pastaOrder = new PastaOrder();
+            pastaOrder.PastaName = PastaName;
+            pastaOrder.BasePrice = PastaPrice;
+
+            _context.PastaOrders.Add(pastaOrder);
+            _context.SaveChanges();
         }
     }
 }
